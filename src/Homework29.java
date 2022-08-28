fc8f92f6241c1e94787938097091c8e913de9615
OlyaTelesh <telesh.olya@mail.ru>
2022-08-28 17:17:31 +0300

    Вывести на экран буквы из номера документа в формате "Letters:yyyyyyyy" в верхнем регистре

--- src/Homework29.java
+++ src/Homework29.java
@@ -2,10 +2,16 @@ import java.util.regex.Pattern;
 
 public class Homework29 {
     public static void main(String[] args) {
-        String text = "1234 qwe 4567 pou 5e6t ";
-        String a = text.substring(0,5);
-        String b = text.substring(9,13);
+        String text = "1234 qae 4567 pou 5e6t ";
+        String a = text.substring(0, 5);
+        String b = text.substring(9, 13);
         System.out.println("It's look like" + a.concat(b));
 
+
+        System.out.println(text.replace("a", "*"));
+
+        String q = text.substring(5,8) + text.substring(14,17);
+        System.out.println("Letters:" +q.toUpperCase());
     }
 }
+
