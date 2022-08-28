8ac2a84b0d98ed3f1c436f9a168ca39707e8f18c
OlyaTelesh <telesh.olya@mail.ru>
2022-08-28 16:41:21 +0300

    Создали метод с finally

--- src/Examination.java
+++ src/Examination.java
@@ -26,9 +26,19 @@ public class Examination {
             int[] a = new int[6];
             a[7] = 10;
             System.out.println(a[7]);
-        } catch (ArrayIndexOutOfBoundsException|Error e2){
-        System.out.println("Sorry,now it's " + e2);
+        } catch (ArrayIndexOutOfBoundsException | Error e2) {
+            System.out.println("Sorry,now it's " + e2);
+        }
+        try {
+            int a = 0;
+            int b = 7 / a;
+            System.out.println("Correct, i can do it");
+        } catch (ArithmeticException e) {
+            System.out.println("Sorry, it's " + e);
+        }
+        finally {
+            System.out.println("Prostotak");
+        }
     }
-}
 }
 
