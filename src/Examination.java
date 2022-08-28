04cc939fe2f7ccd74f1bba79285df2860f71e85f
OlyaTelesh <telesh.olya@mail.ru>
2022-08-28 16:37:09 +0300

    создали еще два метода для мультипроверки catch и двойной проверки (два catch)

--- src/Examination.java
+++ src/Examination.java
@@ -1,2 +1,34 @@
+import java.io.IOException;
+
 public class Examination {
+    public static void main(String[] args) {
+
+
+        try {
+            int a = 0;
+            int b = 7 / a;
+            System.out.println("Correct, i can do it");
+        } catch (ArithmeticException e) {
+            System.out.println("Sorry, it's " + e);
+        }
+
+        try {
+            int a = 0;
+            int b = 7 / a;
+            System.out.println("Correct, i can do it");
+        } catch (ArithmeticException e) {
+            System.out.println("Sorry, it's " + e);
+        } catch (ArrayStoreException e1) {
+            System.out.println("Sorry, it's " + e1);
+        }
+
+        try {
+            int[] a = new int[6];
+            a[7] = 10;
+            System.out.println(a[7]);
+        } catch (ArrayIndexOutOfBoundsException|Error e2){
+        System.out.println("Sorry,now it's " + e2);
+    }
 }
+}
+
